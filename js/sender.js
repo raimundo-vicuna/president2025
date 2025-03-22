71247c2e2d07d6bb453d72f744c8d544982c5845
raideus138 <raivicuna@gmail.com>
2025-03-22 18:38:01 -0300

    clean css and js/gitignore

--- js/sender.js
+++ js/sender.js
@@ -1,16 +1,18 @@
-const notification_from = document.querySelector('.notification_from')
+const notification_from = document.querySelector('.notification_from');
 const button = document.querySelector('.send_bt');
+const inputName = document.querySelector('.name_camp');
+const inputMessage = document.querySelector('.message_camp');
 
 const SERVICE_ID = 'service_vgg1lo1';
 const TEMPLATE_ID = 'template_tz6bxlg';
-const PUBLIC_KEY = '0u6BafWDWeuc7jcke'; 
+const PUBLIC_KEY = '0u6BafWDWeuc7jcke';
 
 function sendMail() {
-    const userName = document.querySelector('.mail_text').value.trim();
-    const userMessage = document.querySelector('.massage-bt').value.trim();
+    const userName = inputName.value.trim();
+    const userMessage = inputMessage.value.trim();
 
     if (!userName || !userMessage) {
-        notification_from.style.color = 'red'
+        notification_from.style.color = 'red';
         notification_from.textContent = 'Completa los campos';
         return;
     }
@@ -21,17 +23,23 @@ function sendMail() {
         {
             from_name: userName,  
             message: userMessage, 
-            reply_to: 'raimundovicunap26@alumnos.cph-cja.cl, franciscopuelmac26@alumnos.cph-cja.cl' 
+            to_email: "raimundovicunap26@alumnos.cph-cja.cl, franciscopuelmac26@alumnos.cph-cja.cl"
         },
         PUBLIC_KEY
     )
     .then((response) => {
         notification_from.style.color = 'green';
         notification_from.textContent = 'ENVIADO'; 
+        
+        inputName.value = '';
+        inputMessage.value = '';
     })
     .catch((error) => {
         notification_from.style.color = 'red';
-        notification_from.textContent = 'Intentalo nuevamente';
+        notification_from.textContent = 'Inténtalo nuevamente';
+        
+        inputName.value = '';
+        inputMessage.value = '';
     });
 }
 
